dd83aa05d32e3cb4c40029adb0d0c9db0e6f6d45
Jacob Hummer <jcbhmr@outlook.com>
2023-03-10 22:13:40 +0000

    Update next.js config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack(cfg) {
-    cfg.optimization.minimize = false;
-    return cfg;
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
